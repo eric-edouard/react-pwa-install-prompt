0650c9c769061b8b2103e4d6376d34f6a77e7e2d
Eric <eric.amilhat@epitech.eu>
2020-10-04 15:48:49 +0200

    usePWA + prettierrc config

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,67 @@
 import * as React from 'react'
-import styles from './styles.module.css'
 
-interface Props {
-  text: string
+const checkStandalone = () => {
+  return (
+    // @ts-ignore
+    navigator?.standalone ||
+    window?.matchMedia('(display-mode: standalone)').matches
+  )
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+type PwaInfos = {
+  isInstallPromptSupported: boolean
+  promptInstall: () => Promise<boolean> | null
+  isStandalone: boolean
 }
+
+const initialState: PwaInfos = {
+  isInstallPromptSupported: false,
+  promptInstall: () => null,
+  isStandalone: checkStandalone()
+}
+
+const usePWA = () => {
+  const [pwaInfos, setPwaInfos] = React.useState<PwaInfos>(initialState)
+
+  React.useEffect(() => {
+    const beforeinstallpromptHandler = (e: Event) => {
+      e.preventDefault()
+
+      const promptInstall = async () => {
+        // @ts-ignore
+        const promptRes = await e.prompt()
+        if (promptRes.outcome === 'accepted') {
+          setPwaInfos({
+            ...pwaInfos,
+            isStandalone: checkStandalone()
+          })
+          return true
+        }
+        return false
+      }
+
+      setPwaInfos({
+        isInstallPromptSupported: true,
+        promptInstall,
+        isStandalone: checkStandalone()
+      })
+    }
+
+    const onAppInstalled = () => {
+      setTimeout(() => setPwaInfos({ ...pwaInfos, isStandalone: checkStandalone() }), 200)
+    }
+
+    window.addEventListener('beforeinstallprompt', beforeinstallpromptHandler)
+    window.addEventListener('appinstalled', onAppInstalled)
+
+    return () => {
+      // Cleanup event listeners
+      window.removeEventListener('beforeinstallprompt', beforeinstallpromptHandler)
+      window.removeEventListener('appinstalled', onAppInstalled)
+    }
+  }, [pwaInfos])
+
+  return pwaInfos
+}
+
+export default usePWA
